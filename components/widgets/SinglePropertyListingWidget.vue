12f17126a8d3774f59fe38dad9f0876f516fdf23
Oyinbo David <dev.davexoyinbo@gmail.com>
2021-09-29 23:38:47 +0100

    Delete property

--- components/widgets/SinglePropertyListingWidget.vue
+++ components/widgets/SinglePropertyListingWidget.vue
@@ -7,19 +7,25 @@
       </div>
       <div class="sd-list-right">
         <h4 class="listing_dashboard_title">
-          <a href="#" class="theme-cl">{{property.title}}</a>
+          <a href="#" class="theme-cl">{{ property.title }}</a>
         </h4>
-        <div class="user_dashboard_listed">Price: N {{price}} {{property.status == "RENT" ? '/ yr' : ''}}</div>
+        <div class="user_dashboard_listed">
+          Price: N {{ price }} {{ property.status == "RENT" ? "/ yr" : "" }}
+        </div>
         <div class="user_dashboard_listed">
           Listed in
-          <span class="text-uppercase">{{property.status == "RENT" ? 'rentals' : 'sales'}}</span> and 
-          <span>{{property.type}}</span>
+          <span class="text-uppercase">{{
+            property.status == "RENT" ? "rentals" : "sales"
+          }}</span>
+          and
+          <span>{{ property.type }}</span>
           <!-- <a href="javascript:void(0);" class="theme-cl">Rentals</a> and
           <a href="javascript:void(0);" class="theme-cl">Apartments</a> -->
         </div>
         <div class="user_dashboard_listed">
-          City: <a href="javascript:void(0);" class="theme-cl">{{property.city}}</a> ,
-          Area: {{property.area}} sq m
+          City:
+          <a href="javascript:void(0);" class="theme-cl">{{ property.city }}</a>
+          , Area: {{ property.area }} sq m
         </div>
         <div class="action">
           <a href="#" data-toggle="tooltip" data-placement="top" title="Edit"
@@ -31,15 +37,8 @@
             data-placement="top"
             title="Delete Property"
             class="delete"
+            @click.prevent="deleteProperty()"
             ><i class="ti-close"></i
-          ></a>
-          <a
-            href="#"
-            data-toggle="tooltip"
-            data-placement="top"
-            title="Make Featured"
-            class="delete"
-            ><i class="ti-star"></i
           ></a>
         </div>
       </div>
@@ -48,6 +47,9 @@
 </template>
 
 <script>
+import Swal from "sweetalert2";
+// import 'sweetalert2/src/sweetalert2.scss'
+
 export default {
   props: {
     property: {
@@ -69,6 +71,28 @@ export default {
       return Number(
         Number(this.property.price || 0).toFixed(2)
       ).toLocaleString();
+    },
+  },
+  methods: {
+    deleteProperty() {
+      event.preventDefault();
+
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.$axios.delete(`/properties/${this.property.slug}`).then(() => {
+            this.$emit("property-deleted");
+            Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          });
+        }
+      });
     },
   },
 };
--- pages/dashboard/my-properties.vue
+++ pages/dashboard/my-properties.vue
@@ -7,23 +7,28 @@
       </div>
 
       <div class="row">
-        <single-property-listing-widget v-for="property in properties" :key="property.id" :property="property" />
+        <single-property-listing-widget
+          @property-deleted="propertyDeleted(property.id)"
+          v-for="property in properties"
+          :key="property.id"
+          :property="property"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import SinglePropertyListingWidget from '~/components/widgets/SinglePropertyListingWidget.vue';
+import SinglePropertyListingWidget from "~/components/widgets/SinglePropertyListingWidget.vue";
 export default {
   components: { SinglePropertyListingWidget },
   layout: "dashboard",
   data() {
     return {
       propertiesPagination: {
-        data: []
-      }
-    }
+        data: [],
+      },
+    };
   },
   async asyncData({ $axios }) {
     let propertiesPagination = {};
@@ -46,8 +51,23 @@ export default {
   },
   computed: {
     properties() {
-      return this.propertiesPagination.data || []
-    }
-  }
+      return this.propertiesPagination.data || [];
+    },
+  },
+  methods: {
+    propertyDeleted(id) {
+      const index = this.properties.findIndex((el) => el.id == id);
+
+      if (index < 0) return;
+
+      let properties = [...this.properties]
+
+      properties.splice(index, 1);
+
+      Object.assign(this.propertiesPagination, {data: properties})
+
+      // debugger
+    },
+  },
 };
 </script>
